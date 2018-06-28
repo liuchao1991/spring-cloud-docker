62e167305f9bc0bc116fe113a4ef91f4b71f0702
liuchao1991 <liuchao2019@gmail.com>
2018-06-28 22:38:42 +0800

    Spring Cloud学习第二天

---
+++ microservice-discovery-eureka-ha/src/main/java/com/liuchao/cloud/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.liuchao.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// 声明这是一个Eureka Server
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
